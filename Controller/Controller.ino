8e49acb9e06471961330b6e0690f06b546691a2c
Unknown <jonas.de.ridder@student.howest.be>
2018-05-02 08:58:02 +0200

    Potentio Meter hooked up
    currently just Serial.write("") ing everything

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,5 +1,7 @@
 #define tilt 2
+#define potPin A0
 bool tilted = false;
+int potVal = 0;
 
 unsigned long previousMillis = 0;
 const long interval = 1000;
@@ -7,13 +9,16 @@ const long interval = 1000;
 void setup() {
   // put your setup code here, to run once:
   pinMode(tilt, INPUT);
+  pinMode(potPin, INPUT);
   attachInterrupt(digitalPinToInterrupt(tilt), toggleMoving, CHANGE);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  potVal = analogRead(potPin);
+  Serial.print(potVal);
+  Serial.write("\n");
 }
 
 void toggleMoving(){
