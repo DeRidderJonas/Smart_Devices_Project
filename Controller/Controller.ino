d0234ffa428152794e84a5ece2e97af621a79a77
Unknown <jonas.de.ridder@student.howest.be>
2018-05-08 17:39:58 +0200

    Arduino code added (not yet tested)
    code toegevoegd die nog niet getest is (zat op trein)

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,16 +1,26 @@
 #define tilt 2
+#define changeDir 3
+#define startStop 4
 #define potPin A0
+bool left = true;
 bool tilted = false;
+bool playing = false;
 int potVal = 0;
 
-unsigned long previousMillis = 0;
+unsigned long previousMillisTilt = 0;
+unsigned long previousMillisDir = 0;
+unsigned long previousMillisStartStop = 0;
 const long interval = 1000;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(tilt, INPUT);
+  pinMode(changeDir, INPUT);
+  pinMode(startStop, INPUT);
   pinMode(potPin, INPUT);
   attachInterrupt(digitalPinToInterrupt(tilt), toggleMoving, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(changeDir), toggleDir, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(startStop), toggleOn, CHANGE);
   Serial.begin(9600);
 }
 
@@ -21,12 +31,49 @@ void loop() {
   Serial.write("\n");
 }
 
-void toggleMoving(){
+void buttonPressed(int button){
   unsigned long currentMillis = millis();
-  if(currentMillis - previousMillis >= interval){
-    previousMillis = currentMillis;
-    tilted = !tilted;
-    Serial.write("tilted");
+  unsigned long previousMillis;
+  switch(button){
+    case 0:
+      previousMillis = previousMillisTilt;
+      if(currentMillis - previousMillisTilt >= interval){
+        previousMillisTilt = currentMillis;
+        tilted = !tilted;
+        Serial.write("tilted " + tilted);
+      }
+      break;
+    case 1:
+      previousMillis = previousMillisDir;
+      if(currentMillis - previousMillisDir >= interval){
+        previousMillisDir = currentMillis;
+        left = !left;
+        Serial.write("toggleDir " + left);
+      }
+      break;
+    case 2:
+      previousMillis = previousMillisStartStop;
+      if(currentMillis - previousMillisStartStop >= interval){
+        previousMillisStartStop = currentMillis;
+        playing = !playing;
+        Serial.write("togglePlaying " + playing);
+      }
+      break;
   }
+  if(currentMillis - previousMillis >= interval){
+    
+  }
+}
+
+void toggleMoving(){
+  buttonPressed(0);
+}
+
+void toggleDir(){
+  buttonPressed(1);
+}
+
+void toggleOn(){
+  buttonPressed(2);
 }
 
--- Displayer/Displayer.ino
+++ Displayer/Displayer.ino
@@ -7,11 +7,17 @@ SoftwareSerial mySerial(3,4);
 #define d5 6
 #define d6 7
 #define d7 8
-LiquidCrystal lcd(rs,enable,d4,d5,d6d,7);
+LiquidCrystal lcd(rs,enable,d4,d5,d6,d7);
+#define buzzer 12
+#define winLED 1
+#define lossLED 2
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16,2);
   Serial.begin(9600);
+  pinMode(buzzer, OUTPUT);
+  pinMode(winLED, OUTPUT);
+  pinMode(lossLED, OUTPUT);
   while(!Serial){
     
   }
@@ -24,9 +30,42 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if(mySerial.available()){
-    Serial.write(mySerial.read());
+    int inByte = mySerial.read();
+    Serial.write(inByte);
+    lcd.clear();
+    switch(inByte){
+      case 'w':
+        playVictorySound();
+        digitalWrite(winLED, HIGH);
+        lcd.write("winner winner");
+        lcd.setCursor(0,1);
+        lcd.write("Chicken dinner!");
+        break;
+      case 'l':
+        playLosingSound();
+        digitalWrite(lossLED, HIGH);
+        lcd.write("You lose!");
+        break;
+      case 'r':
+        lcd.write("reseting");
+        digitalWrite(winLED, LOW);
+        digitalWrite(lossLED, LOW);
+        break;  
+      default:
+        lcd.write(inByte);
+        break;  
+    }
   }
   if(Serial.available()){
     mySerial.write(Serial.read());
   }
 }
+
+void playVictorySound(){
+  //you know what to do...
+}
+
+void playLosingSound(){
+  //you know...
+}
+
