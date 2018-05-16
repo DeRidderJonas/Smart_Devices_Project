909989653755ba7145227dfec34cffc3347b352d
TackJordy <jordy.tack@howest.student.be>
2018-05-16 08:36:26 +0200

    Controller adjustments

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,10 +1,14 @@
 #define startStop 2
 #define tilt 3
 #define potPin A0
-bool left = true;
+#define ledLeft 12
+#define ledUp 13
+#define ledRight 10
+#define ledDown 11
 bool tilted = false;
 bool playing = false;
 int potVal = 0;
+int dir = 0; //0 achteruit; 1 links; 2 vooruit; 3 rechts
 
 unsigned long previousMillisTilt = 0;
 unsigned long previousMillisDir = 0;
@@ -16,6 +20,10 @@ void setup() {
   pinMode(tilt, INPUT);
   pinMode(startStop, INPUT);
   pinMode(potPin, INPUT);
+  pinMode(ledLeft,OUTPUT);
+  pinMode(ledUp,OUTPUT);
+  pinMode(ledRight,OUTPUT);
+  pinMode(ledDown,OUTPUT);
   attachInterrupt(digitalPinToInterrupt(tilt), toggleMoving, RISING);
   attachInterrupt(digitalPinToInterrupt(startStop), toggleOn, RISING);
   Serial.begin(9600);
@@ -24,10 +32,38 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   potVal = analogRead(potPin);
-  if(potVal<=512){
-    Serial.print("left");
-  } else {
-    Serial.print("right");
+
+  
+  if(potVal<=128){   
+    dir = 0;
+    digitalWrite(ledUp, LOW);
+    digitalWrite(ledLeft, LOW);
+    digitalWrite(ledRight, LOW);
+    digitalWrite(ledDown,HIGH);
+  } else if(potVal<=384) {
+    dir = 1;
+    digitalWrite(ledUp, LOW);
+    digitalWrite(ledLeft, HIGH);
+    digitalWrite(ledRight, LOW);
+    digitalWrite(ledDown,LOW);
+  }else if(potVal<=640) {
+    dir = 2;
+    digitalWrite(ledUp, HIGH);
+    digitalWrite(ledLeft, LOW);
+    digitalWrite(ledRight, LOW);
+    digitalWrite(ledDown,LOW);
+  }else if(potVal<=896) {
+    dir = 3;
+    digitalWrite(ledUp, LOW);
+    digitalWrite(ledLeft, LOW);
+    digitalWrite(ledRight, HIGH);
+    digitalWrite(ledDown,LOW);
+  }else {
+    dir = 0;
+    digitalWrite(ledUp, LOW);
+    digitalWrite(ledLeft, LOW);
+    digitalWrite(ledRight, LOW);
+    digitalWrite(ledDown,HIGH);
   }
   
 }
@@ -42,10 +78,10 @@ void buttonPressed(int button){
         previousMillisTilt = currentMillis;
         tilted = !tilted;
         Serial.print("tilted: ");
-        Serial.print(tilted);
+        Serial.print(dir);
       }
       break;
-      case 1:
+    case 1:
       previousMillis = previousMillisStartStop;
       if(currentMillis - previousMillisStartStop >= interval){
         previousMillisStartStop = currentMillis;
